af977e10d9eaba77f412647cf9d7bf9fb477e52a
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-01-12 20:35:51 +0200

    added slick slider to news block

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    // $('.header__text-block').slick({
+    //     autoplay: true,
+    //     infinite: true,
+    //     // speed: 300,
+    //     slidesToShow: 1,
+    //     adaptiveHeight: true,
+    //     dots: true,
+
+    // });
+
+    $('.gallery').slick({
+        autoplay: true,
+        speed: 3000,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+    });
+
+});
+
+
